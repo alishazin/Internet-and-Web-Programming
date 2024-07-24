ae5d76ae3c071682a06d9ef6da82bb4e46659b39
alishazin <78612608+alishazin@users.noreply.github.com>
2024-07-24 11:51:54 +0530

    Add files via upload

--- 1/script.js
+++ 1/script.js
@@ -1,1 +1,17 @@
 console.log("Hello World in Console")
+
+const string = "HELLO WORLD";
+
+for (let i=0; i<string.length; i++) {
+	setTimeout(() => {
+		const span = document.createElement('span');
+		span.innerText = string[i];
+		document.querySelector('h1').appendChild(span);
+		
+		if (i == string.length - 1) {
+			setTimeout(() => {
+				document.querySelector('#cursor').remove();
+			}, 3000);
+		}
+	}, i * 1000);		
+}
