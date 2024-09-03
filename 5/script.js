70d5c64139e6a0948b6395f6ec17311bdeb2849b
alishazin <78612608+alishazin@users.noreply.github.com>
2024-09-03 17:45:59 +0530

    5th is done

--- 5/script.js
+++ 5/script.js
@@ -1,6 +1,15 @@
+
+const imgSources = [
+    "https://www.fcbarcelona.com/photo-resources/2020/02/24/3f1215ed-07e8-47ef-b2c7-8a519f65b9cd/mini_UP3_20200105_FCB_VIS_View_1a_Empty.jpg?width=1200&height=750",
+    "https://www.fcbarcelona.com/fcbarcelona/photo/2022/10/23/95c05ed8-7b4a-4563-87ea-9085f9621385/VIC_1659.jpg",
+    "https://www.fcbarcelona.com/photo-resources/2022/11/22/f79428ec-55c3-4dbd-8776-035f5ccc2147/comunicat.jpg?width=1200&height=750",
+    "https://cosxeuwlta.cloudimg.io/_outsports-prodweb_/uploads/2024/02/usatsi-21109333-scaled.jpg?auto=format&auto=compress&gravity=smart"
+]
+
 function onLoad() {
     
     setTimeInterval()
+    setIntervalForImg()
 
 }
 
@@ -15,4 +24,19 @@ function setTimeInterval() {
         const datetime = new Date()
         time.innerText = `${affixZero(datetime.getHours())}:${affixZero(datetime.getMinutes())}:${affixZero(datetime.getSeconds())}`
     }, 1000)
+}
+
+function setIntervalForImg() {
+    
+    let count = 0;
+    let length = imgSources.length;
+    const element = document.querySelector(".post-box img.post");
+
+    function func() {
+        element.src = imgSources[count];
+        count = (count + 1) % length;
+    }
+    
+    func()
+    setInterval(func, 5000);
 }
