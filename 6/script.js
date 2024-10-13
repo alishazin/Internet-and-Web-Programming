0742a41be1832f89871bf5dcb35895396a00b80c
alishazin <78612608+alishazin@users.noreply.github.com>
2024-10-13 17:15:07 +0530

    6 done

--- 6/script.js
+++ 6/script.js
@@ -4,26 +4,35 @@ function submit() {
     
     const contactField = document.querySelector('input#contact')
     const contactErr = document.querySelector('div.textfield.contact .err-msg')
+    contactErr.innerHTML = ''
     
     const zipField = document.querySelector('input#zip')
     const zipErr = document.querySelector('div.textfield.zip .err-msg')
+    zipErr.innerHTML = ''
 
     if (!(/^[0-9]*$/.test(contactField.value))) {        
         contactErr.innerHTML = 'Contact Number must only contain numbers.'
+        return;
     }
-
+    
     if (!(/^[0-9]*$/.test(zipField.value))) {
         zipErr.innerHTML = 'Zip Code must only contain numbers.'
+        return;
     }
     
     redirect()
 
 }
 
+function refresh() {
+    console.log(1000);
+    window.location.reload()
+}
+
 function redirect() {
 
     const main = document.querySelector('main')
-    main.innerHTML = `<h1>XYZ Industries</h1><h2>Application Form</h2><p>Thank you for the response</p><button onclick='() => window.location.reload()'>Submit another response</button>`
+    main.innerHTML = `<h1>XYZ Industries</h1><h2>Application Form</h2><p>Thank you for the response</p><button onclick="refresh()">Submit another response</button>`
 
 }
 
